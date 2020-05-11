f18d99257a6945a035d5afca7209aa0b87e01840
gabrielrmachado <gabriel.rmachado10@gmail.com>
2020-05-11 13:51:02 -0300

    Code reviewed.

--- index.c
+++ index.c
@@ -9,7 +9,6 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include <stdbool.h>
 #include "index.h"
 
 typedef struct registry
@@ -20,23 +19,79 @@ typedef struct registry
     struct registry* next; // Tratamento de colisões: encadeamento separado (separate chaining).
 } Registry;
 
+typedef struct keyword
+{
+    char keyword[BUFF_SIZE];
+    struct keyword* next;
+} Keyword;
+
 struct index
 {
     int numKeywords;
     int numLinesTextFile;
-    char* keywords[BUFF_SIZE];
+    char* textFile;
+    Keyword* keywords;
     Registry** hash_table;
 };
+
+static short insert_keyword(Index** idx, const char* keyword)
+{
+    short found = 0;
+    Keyword** kw = (&(*idx)->keywords);
+
+    for (int i = 0; i < (*idx)->numKeywords; i++)
+    {
+        if (strcmp((*kw)->keyword, keyword) == 0)
+        {
+            found = 1; break;
+        }
+        else *kw = (*kw)->next;
+    }
+
+    if (found == 0)
+    {
+        Keyword* new_kw = (Keyword*)malloc(sizeof(Keyword));
+        strcpy(new_kw->keyword, keyword);
+
+        if ((*idx)->numKeywords == 0)
+            *kw = new_kw;
+        else
+            (*kw)->next = new_kw;
+
+        new_kw->next = NULL;
+        (*idx)->numKeywords++;
+    }
+
+    return found;
+}
 
 static int compare(const void* a, const void* b)
 {
     return strcmp(*(const char**)a, *(const char**)b);
 }
 
-static int hash_function(char* key, int TABLE_SIZE)
+static int compare_int(const void* a, const void* b)
+{
+    return (*(int*)a - *(int*)b);
+}
+
+static void get_array_keywords(Index* idx, char** keywords[BUFF_SIZE])
+{
+    Keyword* kw = idx->keywords;
+    for (int i = 0; i < idx->numKeywords; i++)
+    {
+        strcpy(keywords[i], kw->keyword);
+        kw = kw->next;
+    }
+
+    // coloca o vetor contendo as palavras-chave em ordem alfabética.
+    qsort(idx->keywords, idx->numKeywords, sizeof(const char*), compare);
+}
+
+
+static int hash_function(const char* key, int TABLE_SIZE)
 {
     int sum = 0, len = strlen(key);
-
     for (int i = 0; i < len; i++)
         sum += (int)key[i];
 
@@ -49,6 +104,8 @@ int index_createfrom(const char* key_file, const char* text_file, Index** idx)
     *idx = (Index*)malloc(sizeof(Index));
     (*idx)->numKeywords = 0;
     (*idx)->numLinesTextFile = 0;
+    (*idx)->keywords = NULL;
+    (*idx)->textFile = malloc(sizeof(char) * strlen(text_file)); strcpy((*idx)->textFile, text_file);
 
     // conta o número de linhas de texto em 'text_file.txt'.
     FILE* file = fopen(text_file, "r");
@@ -71,28 +128,26 @@ int index_createfrom(const char* key_file, const char* text_file, Index** idx)
 
         // conta o número de palavras-chave para calcular um valor de tableSize.
         while (fgets(str, BUFF_SIZE, file) != NULL)
-            (*idx)->numKeywords++;
+        {
+            // remove o '\n' da string.
+            char* pos = strchr(str, '\n');
+            if (pos != NULL) *pos = '\0';
+            insert_keyword(idx, str);
+        }
 
         fclose(file);
-
         (*idx)->hash_table = (Registry**)malloc(sizeof(Registry*) * (*idx)->numKeywords);
 
         for (int i = 0; i < (*idx)->numKeywords; i++)
             (*idx)->hash_table[i] = NULL;
 
-        // lê novamente o arquivo 'keys_file.txt', desta vez para armazenar as chaves no índice.
-        file = fopen(key_file, "r");
-        rewind(file);
-        int i = 0;
 
-        while (fgets(str, BUFF_SIZE, file) != NULL)
+        char keywords[(*idx)->numKeywords][BUFF_SIZE];
+        get_array_keywords(idx, keywords);
+
+        for (int i = 0; i < (*idx)->numKeywords; i++)
         {
-            // remove o '\n' da string.
-            char* pos = strchr(str, '\n');
-            if (pos != NULL) *pos = '\0';
-
-            (*idx)->keywords[i] = malloc(strlen(str) + 1);
-            strcpy((*idx)->keywords[i], str); i++;
+            char str[BUFF_SIZE]; strcpy(str, keywords[i]);
 
             // calcula a chave a partir da palavra-chave fornecida.
             int hash_idx = hash_function(str, (*idx)->numKeywords);
@@ -128,13 +183,53 @@ int index_createfrom(const char* key_file, const char* text_file, Index** idx)
         return 1;
     }
 
+    Keyword* kw = (*idx)->keywords;
+    for (int i = 0; i < (*idx)->numKeywords; i++)
+    {
+        index_put(*idx, kw->keyword);
+        kw = kw->next;
+    }
+
+    return 0;
+}
+int index_get(const Index* idx, const char* key, int** occurrences, int* num_ocurrences)
+{
+    int hash = hash_function(key, idx->numKeywords);
+    Registry* reg = idx->hash_table[hash];
+
+    while (reg != NULL && strcmp(reg->keyword, key) != 0)
+        reg = reg->next;
+
+    if (reg == NULL) return 1;
+    else
+    {
+        *num_ocurrences = reg->numOccurrences;
+        *occurrences = (int*)malloc(sizeof(int) * idx->numLinesTextFile);
+        memset(*occurrences, -1, sizeof(int) * idx->numLinesTextFile);
+
+        // copia o vetor de ocorrência contido no índice para o parâmetro 'occurrences'.
+        for (int i = 0; i < idx->numLinesTextFile; i++)
+        {
+//            printf("GET - occurrences %s: %d\n", key, reg->line_occurrence[i]);
+            (*occurrences)[i] = reg->line_occurrence[i];
+        }
+
+        return 0;
+    }
+}
+int index_put(const Index* idx, const char* key)
+{
     // com as keywords inseridas, agora é hora de ler o texto contido em 'text_file.txt'.
-    file = fopen(text_file, "r");
-    rewind(file);
+    FILE* file = fopen(idx->textFile, "r");
+    rewind(file); int ans = -1;
 
     if (file != NULL)
     {
         int current_line = 0;
+        int num_occurrences = 0;
+        int* occurrences = (int*)malloc(sizeof(int) * idx->numLinesTextFile);
+        memset(occurrences, -1, sizeof(int) * idx->numLinesTextFile);
+
         char str[TEXT_BUFF_SIZE] = "\0";
         while (fgets(str, TEXT_BUFF_SIZE, file) != NULL)
         {
@@ -144,94 +239,101 @@ int index_createfrom(const char* key_file, const char* text_file, Index** idx)
             int i = 0;
             while (i < strlen(str))
             {
-                char word[BUFF_SIZE] = "\0";
-                int j = 0;
+                char word[BUFF_SIZE] = "\0"; int j = 0;
+                if (isspace(str[i])) { i++; continue; }
 
-                // verifica se o caractere atual é uma letra. Se não for, o mesmo é ignorado.
-                if (!isalpha(str[i])) i++;
+                while (!isspace(str[i]))
+                    word[j++] = str[i++];
 
-                // Se for, executa um loop para capturar as demais letras que formam a palavra.
-                // Esse loop é executado até que um caractere não alfabético surja.
-                else
+                if (strcmp(word, key) == 0) // encontrou keyword no texto.
                 {
-                    while (isalpha(str[i]) && i < strlen(str) && j < BUFF_SIZE-1)
-                    {
-                        word[j] = str[i];
-                        i++; j++;
-                    }
+                    num_occurrences++;
 
-                    // verifica se a keyword já existe no índice.
-                    int h_idx = hash_function(word, (*idx)->numKeywords);
-                    Registry* reg = (*idx)->hash_table[h_idx];
-
-                    while (reg != NULL)
-                    {
-                        if (strcmp(word, reg->keyword) == 0)
-                        {
-                            // encontrou a keyword.
-                            reg->line_occurrence[reg->numOccurrences++] = current_line;
-                            break;
-                        }
-                        reg = reg->next;
-                    }
+                    // assegura que a atual linha de texto não seja repetida várias vezes em 'occurrences'.
+                    if (occurrences[current_line-1] == -1)
+                        occurrences[current_line-1] = current_line;
                 }
             }
         }
+
+        if (num_occurrences > 0)
+        {
+            // procura a keyword no índice remissivo.
+            int hash = hash_function(key, idx->numKeywords);
+            Registry* reg = idx->hash_table[hash];
+            Registry* antReg = reg;
+
+            while (reg != NULL && strcmp(reg->keyword, key) != 0)
+            {
+                antReg = reg;
+                reg = reg->next;
+            }
+
+            if (reg == NULL) // não encontrou a keyword.
+            {
+                Registry* newReg = (Registry *) malloc(sizeof(Registry));
+                strcpy(newReg->keyword, key);
+                newReg->numOccurrences = num_occurrences;
+
+                for (int i = 0; i < idx->numLinesTextFile; i++)
+                    newReg->line_occurrence[i] = occurrences[i];
+
+                // primeiro registro no slot.
+                if (antReg == NULL)
+                    idx->hash_table[hash] = newReg;
+
+                else
+                {
+                    antReg->next = newReg;
+                    newReg->next = NULL;
+                }
+
+                // atualiza o vetor de keywords.
+                insert_keyword(idx, key);
+            }
+            else
+            {
+                reg->numOccurrences = num_occurrences;
+                for (int i = 0; i < idx->numLinesTextFile; i++)
+                    reg->line_occurrence[i] = occurrences[i];
+            }
+
+            free(occurrences);
+        }
+        ans = 0;
     }
     else
     {
-        fprintf(stderr, "\nFile %s not found.", text_file);
-        return 1;
+        fprintf(stderr, "\nFile %s not found.", idx->textFile);
+        ans = 1;
     }
-
-    return 0;
+    fclose(file);
+    return ans;
 }
-int index_get(const Index* idx, const char* key, int** occurrences, int* num_ocurrences)
-{
-    int hash = hash_function(key, idx->numKeywords);
-    bool found = false;
 
-    Registry* reg = idx->hash_table[hash];
-
-    while (strcmp(reg->keyword, key) != 0 && reg != NULL)
-        reg = reg->next;
-
-    if (reg == NULL) return 1;
-    else
-    {
-        *num_ocurrences = reg->numOccurrences;
-        *occurrences = (int*)malloc(sizeof(int) * (*num_ocurrences));
-
-        // copia o vetor de ocorrência contido no índice para o parâmetro 'occurrences'.
-        for (int i = 0; i < (*num_ocurrences); i++)
-            (*occurrences)[i] = reg->line_occurrence[i];
-
-        return 0;
-    }
-}
-int index_put(const Index* idx, const char* key)
-{
-    return 0;
-}
 int index_print(const Index* idx)
 {
-    // coloca o vetor contendo as palavras-chave em ordem alfabética.
-    qsort(idx->keywords, idx->numKeywords, sizeof(const char*), compare);
+    char* keywords[idx->numKeywords][BUFF_SIZE];
+    get_array_keywords(idx, &keywords);
 
     // a partir do vetor de keywords ordenado, são feitas as consultas na tabela hash.
     for (int i = 0; i < idx->numKeywords; i++)
     {
-        char keyword[BUFF_SIZE]; strcpy(keyword, idx->keywords[i]);
+        char keyword[BUFF_SIZE]; strcpy(keyword, keywords[i]);
         int* occurrences; int num_occurrences;
         printf("%s: ", keyword);
 
         index_get(idx, keyword, &occurrences, &num_occurrences);
+        qsort(occurrences, idx->numLinesTextFile, sizeof(int), compare_int);
 
-        for (int j = 0; j < num_occurrences-1; j++)
-            printf("%d, ", occurrences[j]);
-
-        printf("%d\n", occurrences[num_occurrences-1]);
-
+        for (int j = 0; j < idx->numLinesTextFile; j++)
+        {
+            if (occurrences[j] != -1)
+            {
+                if (j < idx->numLinesTextFile-1) printf("%d, ", occurrences[j]);
+                else printf("%d\n", occurrences[j]);
+            }
+        }
     }
     return 0;
 }
--- main.c
+++ main.c
@@ -8,6 +8,8 @@ int main(int argc, char** argv)
     argv[2] = "text_file.txt";
     index_createfrom(argv[1], argv[2], &idx);
     index_print(idx);
+    index_put(idx, "read");
+    index_print(idx);
 
     return 0;
 }
