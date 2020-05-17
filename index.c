129fb1234eb00007a55c2640a2312b441d9562c3
gabrielrmachado <gabriel.rmachado10@gmail.com>
2020-05-17 15:11:26 -0300

    Inclusion of test files.

--- index.c
+++ index.c
@@ -349,28 +349,4 @@ int index_print(const Index* idx)
         }
     }
     return 0;
-
-//    while (fgets(str, TEXT_BUFF_SIZE, file) != NULL)
-//    {
-//        current_line++;
-//
-//        // separa as palavras da frase capturada.
-//        int i = 0;
-//        while (i < strlen(str))
-//        {
-//            char word[BUFF_SIZE] = "\0"; int j = 0;
-//            if (!isalpha(str[i])) { i++; continue; }
-//
-//            while (isalpha(str[i]))
-//                word[j++] = str[i++];
-//
-//            if (strcmp(word, key) == 0) // encontrou keyword no texto.
-//            {
-//                num_occurrences++;
-//
-//                // assegura que a atual linha de texto não seja repetida várias vezes em 'occurrences'.
-//                if (occurrences[current_line-1] == -1)
-//                    occurrences[current_line-1] = current_line;
-//            }
-//        }
 }
